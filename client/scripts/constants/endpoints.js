318e2b59b722a0e38f5fa7edc80c61c8645d0467
Arturo Coronel <aoitsu3@gmail.com>
2016-03-19 00:30:13 -0700

    Fix broken styles on series list and on navigation links

--- client/scripts/constants/endpoints.js
+++ client/scripts/constants/endpoints.js
@@ -3,6 +3,6 @@ export const IMAGE_BUCKET = 'http://weetrack.s3.amazonaws.com';
 export const LOADING_IMAGE = 'http://weetrack.s3.amazonaws.com/loading-icon.gif';
 
 /* API endpoints */
-export const SERIES_ENDPOINT = '/api/series';
-export const SEASON_ENDPOINT = '/api/seasons';
-export const FEED_ENDPOINT = '/api/feed';
+export const SERIES_ENDPOINT = 'http://track.datyayu.xyz/api/series';
+export const SEASON_ENDPOINT = 'http://track.datyayu.xyz/api/seasons';
+export const FEED_ENDPOINT = 'http://track.datyayu.xyz/api/feed';
