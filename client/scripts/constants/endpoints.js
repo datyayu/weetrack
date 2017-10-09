dd9c626498ff3d96246812cd60f9379788990568
arturo <aoitsu3@gmail.com>
2017-10-08 23:54:56 -0700

    updated api host

--- client/scripts/constants/endpoints.js
+++ client/scripts/constants/endpoints.js
@@ -2,7 +2,9 @@
 export const IMAGE_BUCKET = 'http://weetrack.s3.amazonaws.com';
 export const LOADING_IMAGE = 'http://weetrack.s3.amazonaws.com/loading-icon.gif';
 
+
 /* API endpoints */
-export const SERIES_ENDPOINT = 'http://track.datyayu.xyz/api/series';
-export const SEASON_ENDPOINT = 'http://track.datyayu.xyz/api/seasons';
-export const FEED_ENDPOINT = 'http://track.datyayu.xyz/api/feed';
+const HOST = '';
+export const SERIES_ENDPOINT = `${HOST}/api/series`;
+export const SEASON_ENDPOINT = `${HOST}/api/seasons`;
+export const FEED_ENDPOINT = `${HOST}/api/feed`;
